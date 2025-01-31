a0a19f7e250a679496bf8d03837abd3b331f8d8a
hamza <hamzahelal17315@hotmail.com>
2025-01-31 22:21:32 +0300

    jss

---
+++ js2.js
@@ -0,0 +1,69 @@
+ //  function sumArray(numbers) {
+//     let sum = 0;
+
+//     for (let i = 0; i < numbers.length; i++) { 
+//         sum += numbers[i]; 
+//     }
+
+//     return sum;
+// }
+
+// let myNumbers = [10, 100, 105, 100]; 
+// document.write(sumArray(myNumbers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bignumber(big) {
+// let num = big[0];
+
+//    for (let i = 1; i < big.length; i++) { 
+//        if (big[i] > num) { 
+//     num = big[i];
+// }
+//  }
+//  return num;
+// }
+
+// let mybigNumber = [100, 200, 300, 900]; 
+// document.write(bignumber(mybigNumber));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum (x,y,z,i,o,f){
+//   return x+y+z+i+o+f;
+// }
+
+// var average = function avg (x,y,z,i,o,f){
+//   return sum(x,y,z,i,o,f)/6;
+// }
+
+// document.writeln(average(10,20,30,40,50,60)) 
+
+
+
+
+
